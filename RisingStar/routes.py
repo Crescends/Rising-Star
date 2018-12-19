f053831d77e4950a05bd1adb5e553a8a2cc9e47d
Edith Oyugi <edithoyu@gmail.com>
2018-12-19 00:52:11 -0600

    Maybe

--- RisingStar/routes.py
+++ RisingStar/routes.py
@@ -1,6 +1,6 @@
-from app import app
+from .app import app
 from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
---
+++ run.py
@@ -0,0 +1,3 @@
+from RisingStar import app
+
+app.app.run(port=5000, debug=True)
