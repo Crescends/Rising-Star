2bf75595568c1fc7bd5fec47a2640977402291da
DubiskiTSA <ajanakufaith0@gmail.com>
2019-01-11 16:31:20 -0600

    Fixed some typos

--- RisingStar/models/merch.py
+++ RisingStar/models/merch.py
@@ -1,15 +1,14 @@
 from .base import db
 
-class Merchandise(db.model):
+class Merchandise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(20), unique=True, nulable=False)
-    name = db.Column(db.Integer, nulable=False) # the display name
-    cost = db.Column(db.Float, nulable=False, default=0)
-    image_name = db.Column(db.String(25), nulable=False)
+    type = db.Column(db.String(20), unique=True, nullable=False)
+    name = db.Column(db.Integer, nullable=False) # the display name
+    cost = db.Column(db.Float, nullable=False, default=0)
+    image_name = db.Column(db.String(25), nullable=False)
 
 def add_merch(db):
     db.session.add(Merchandise(type="hoodie", name="Black Campfire Jacket", cost=30.00, image_name="hoodie1.jpeg"))
     db.session.add(Merchandise(type="bandana", name="Black Campfire Jacket", cost=2.50, image_name="bandana1.jpg"))
     db.session.add(Merchandise(type=""))
-
-sweatshirts $25, pillows are $5, shirts are $15, tank top is $10. Posters are $9.99 and albums are $29.99
+    
--- RisingStar/models/users.py
+++ RisingStar/models/users.py
@@ -1,14 +1,14 @@
 from .base import db
 
 purchases = db.Table('purchases',
-          db.Column("user_id", db.Integer, db.ForeignKey('user.id'), primary_key=True)
+          db.Column("user_id", db.Integer, db.ForeignKey('user.id'), primary_key=True),
           db.Column("merch_id", db.Integer, db.ForeignKey('merchandise.id'), primary_key=True)
 )
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    full_name = db.Column(db.String(100), unique = false, nullable=false)
+    full_name = db.Column(db.String(100), unique =False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
