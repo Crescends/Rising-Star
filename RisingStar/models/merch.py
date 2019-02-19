bb89a09f89a0bfc1052d3a909879b729d1e2a00d
Denzell Ford <fdenzell@gmail.com>
2019-02-18 22:34:12 -0600

    Added checkout

--- RisingStar/models/merch.py
+++ RisingStar/models/merch.py
@@ -1,21 +1,23 @@
 from .base import db
+import locale
 
+locale.setlocale(locale.LC_ALL, '')
 class Merchandise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(20), unique=False, nullable=False)
     name = db.Column(db.Integer, nullable=True) # the display name
-    cost = db.Column(db.Float, nullable=False, default=0)
+    cost = db.Column(db.String, nullable=False, default=0)
     image_name = db.Column(db.String(25), nullable=False)
 
     def __repr__(self):
         return f"{self.type}: {self.name} costs {self.cost}"
 
-hoodie = Merchandise(type="hoodie", name="Black Campfire Jacket", cost=30.00, image_name="hoodie1.jpeg")
-bandana = Merchandise(type="bandana", name="Black Campfire Bandana", cost=2.50, image_name="bandana1.jpeg")
-pillow = Merchandise(type="pillow", name="Black Campfire Pillow", cost=25, image_name="pillow1.jpeg")
-shirt = Merchandise(type="shirt", name="Black Campfire Shirt", cost=25, image_name="shirt1.jpeg")
-sweat_shirt = Merchandise(type="sweat_shirt", name="Black Campfire Sweat Shirt", cost=25, image_name="sweat_shirt1.jpeg")
-tank = Merchandise(type="tank", name="Black Campfire Tank Top", cost=25, image_name="tank1.jpeg")
+hoodie = Merchandise(type="hoodie", name="Black Campfire Jacket", cost=locale.currency(30.00), image_name="hoodie1.jpeg")
+bandana = Merchandise(type="bandana", name="Black Campfire Bandana", cost=locale.currency(2.50), image_name="bandana1.jpeg")
+pillow = Merchandise(type="pillow", name="Black Campfire Pillow", cost=locale.currency(25), image_name="pillow1.jpeg")
+shirt = Merchandise(type="shirt", name="Black Campfire Shirt", cost=locale.currency(25), image_name="shirt1.jpeg")
+sweat_shirt = Merchandise(type="sweat_shirt", name="Black Campfire Sweat Shirt", cost=locale.currency(25), image_name="sweat_shirt1.jpeg")
+tank = Merchandise(type="tank", name="Black Campfire Tank Top", cost=locale.currency(25), image_name="tank1.jpeg")
 
 def add_merch():
     db.session.add(hoodie)
