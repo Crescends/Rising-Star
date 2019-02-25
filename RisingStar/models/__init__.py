f1e33ebd43c97c75f53a8249afe202ce6e8e146a
Denzell Ford <fdenzell@gmail.com>
2019-02-24 20:08:04 -0600

    test

--- RisingStar/models/__init__.py
+++ RisingStar/models/__init__.py
@@ -8,8 +8,9 @@ def init_app(app: Flask):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     db.init_app(app)
     db.app = app
-    db.create_all()
-    if not merch_exists():
-        add_merch()
+    db.create_all() 
+    Merchandise.__table__.drop()
+    db.session.commit()
+    add_merch()
 
 __all__ = ["db", "User", "Post", "Merchandise"]
