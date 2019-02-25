c3ff64e7fe18ed82d486e2df741f4ccda06e90c9
Denzell Ford <fdenzell@gmail.com>
2019-02-25 08:36:56 -0600

    debug prints

--- setupDatabase.py
+++ setupDatabase.py
@@ -4,6 +4,9 @@ from RisingStar import create_website
 
 app = create_website()
 
-models.base.db.create_all()
+engine = models.base.db.get_engine()
+models.Merchandise.__table__.create(engine)
+print("created merchandise")
 if not models.merch_has_values():
+    print("adding values to merch")
     models.add_merch()
