ae8d5a9fc18e6cf11357892fd60b9cd904d7ad29
Denzell Ford <fdenzell@gmail.com>
2019-02-25 17:42:37 -0600

    added hidden template routing and fixed typo

--- RisingStar/routes/home.py
+++ RisingStar/routes/home.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template
 home_bp = Blueprint('home_bp', __name__, template_folder='templates', static_url_path='static')
 
+@home_bp.route('/template')
+def template():
+    return render_template('layout.html', title="EnterTitleHere")
+
 @home_bp.route('/')
 @home_bp.route('/home')
 def home():
