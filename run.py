c6015c763fee824a8eedc7b6b20ddbdc4cee0aa0
Denzell Ford <fdenzell@gmail.com>
2019-02-26 20:39:35 -0600

    typo

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import RisingStar
-from setup import setupDatase
-setupDatase()
+from setup import setup_datbase
+setup_datbase()
 app = RisingStar.create_website() 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
