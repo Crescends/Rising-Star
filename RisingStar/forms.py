5af85ac0626423f5206a0ffe3d3d06227d99c2ba
Denzell Ford <fdenzell@gmail.com>
2019-03-14 19:25:27 -0500

    added login functionality

--- RisingStar/forms.py
+++ RisingStar/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from RisingStar.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,11 +11,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Join the Forum")
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username is already taken. Please choose a new one")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email is already taken. Please choose a different one")
+
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    remember = BooleanField("  Remember Me")
+    remember = BooleanField("Remember Me")
     submit = SubmitField("Join the Forum")
-
-def init_app(app):
-    app.config['SECRET_KEY']
--- RisingStar/models/users.py
+++ RisingStar/models/users.py
@@ -1,14 +1,18 @@
 from .base import db
+from RisingStar.ext import login_manager
+from flask_login import UserMixin
 
 purchases = db.Table('purchases',
           db.Column("user_id", db.Integer, db.ForeignKey('user.id'), primary_key=True),
-          db.Column("merch_id", db.Integer, db.ForeignKey('merchandise.id'), primary_key=True)
-          )
+          db.Column("merch_id", db.Integer, db.ForeignKey('merchandise.id'), primary_key=True))
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    full_name = db.Column(db.String(100), unique =False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
