1ee23b9a6a9085b10692a4ff80727bdfc192f760
DubiskiTSA <ajanakufaith0@gmail.com>
2019-03-20 11:11:19 -0500

    dabayase

--- RisingStar/models/__init__.py
+++ RisingStar/models/__init__.py
@@ -6,8 +6,9 @@ from .merch import Merchandise, add_merch, merch_exists, merch_has_values
 
 def init_app(app: Flask):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://LVkIsa44od:08SyDIvbeW@remotemysql.com/LVkIsa44od'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://LVkIsa44od:08SyDIvbeW@remotemysql.com/LVkIsa44od'
     db.init_app(app)
     db.app = app
 
+        
 __all__ = ["db", "User", "Post", "Merchandise"]
