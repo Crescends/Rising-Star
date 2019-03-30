0a1100f605499394e2dcd94a85f01212fc330dc7
Denzell Ford <fdenzell@gmail.com>
2019-03-30 17:20:50 -0500

    refactored forms

--- RisingStar/forms.py
+++ RisingStar/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from RisingStar.models import User
-
+from RisingStar.ext import bcrypt
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
@@ -27,15 +29,23 @@ class LoginForm(FlaskForm):
     remember = BooleanField("Remember Me")
     submit = SubmitField("Join the Forum")
 
-class ChangePassword(FlaskForm):
+class ChangePasswordForm(FlaskForm):
     old_password = PasswordField("Old Password", validators=[DataRequired()])
     new_password = PasswordField("New Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm New Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm New Password", validators=[DataRequired(), EqualTo("new_password", message="Passwords do not match")])
     submit = SubmitField("Change Password")
 
-class ChangeUserName():
-    user_name = StringField("New User Name", validators=[DataRequired(), Length(min=2, max=20)])
-    submit = SubmitField("Change User Name")
+    def validate_old_password(self, old_password):
+        if not bcrypt.check_password_hash(current_user.password, old_password.data):
+            raise ValidationError("The Password is incorrect")
 
-class AddProfilePicture():
-    hi: int
+class UpdateAccountForm(FlaskForm):
+    username = StringField("New User Name", validators=[DataRequired(), Length(min=2, max=20)])
+    picture = FileField("Update Profile Picture", validators=[DataRequired(), FileAllowed('jpg', 'png')])
+    submit2 = SubmitField("Update")
+
+    def validate_username(self, username):
+        if current_user.username != username.data:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("Username is already taken. Please choose a new one")
