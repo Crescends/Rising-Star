3995ff22bc64b8bbd971212be3d69c5d9d73386c
Denzell Ford <fdenzell@gmail.com>
2019-03-30 17:20:25 -0500

    updated database

--- RisingStar/models/order.py
+++ RisingStar/models/order.py
@@ -1,14 +1,14 @@
 from .base import db
-
+from datetime import datetime
 purchases = db.Table('purchases',
     db.Column("merch_id", db.Integer, db.ForeignKey('merchandise.id'), primary_key=True),
     db.Column("order_id", db.Integer, db.ForeignKey('order.id'), primary_key=True))
-
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.Integer , db.ForeignKey('user.id'))
     merch = db.relationship('Merchandise', lazy=True,secondary=purchases, backref="order")
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     is_cart = db.Column(db.Boolean, nullable=True, default=True)
 
     def __iter__(self):
--- RisingStar/routes/forum.py
+++ RisingStar/routes/forum.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, flash, redirect, url_for, request
-from RisingStar.forms import RegistrationForm, LoginForm
+from RisingStar.forms import RegistrationForm, LoginForm, ChangePasswordForm, UpdateAccountForm
 from RisingStar.ext import bcrypt
 from RisingStar.models import db, User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+import random
 
 forum = Blueprint('forum', __name__, template_folder='templates', static_url_path='static/forum')
 
@@ -13,7 +14,8 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        default_images = [f"default{i}.jpg" for i in range(1, 5)]
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password, image_file=random.choice(default_images))
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created you are now able to login', 'success')
@@ -47,7 +49,24 @@ def logout():
     return redirect(url_for('home.home'))
 
 
-@forum.route('/account')
+@forum.route('/account', methods=["POST", "GET"])
 @login_required
 def account():
-    return render_template('account.html', title="Account")
+    pass_form = ChangePasswordForm()
+    update_acct = UpdateAccountForm()
+    if pass_form.submit.data and pass_form.validate_on_submit():
+        if bcrypt.check_password_hash(current_user.password, pass_form.old_password.data):
+            current_user.password = bcrypt.generate_password_hash(pass_form.new_password.data).decode("utf-8")
+            flash("Your password has been updated!!!", "success")
+        else:
+            flash("The Password is not correct", "danger")
+        db.session.commit()
+
+    if update_acct.submit2.data and update_acct.validate_on_submit():
+        print('data')
+        current_user.username = update_acct.username.data
+        print(current_user.username)
+        flash('Your username has been updated!', 'success')
+        db.session.commit()
+    img_src = url_for("static", filename=f'images/profile_pics/{current_user.image_file}')
+    return render_template('account.html', title="Account", pssd=pass_form, update=update_acct, pfp=img_src)
