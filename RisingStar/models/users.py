bdf3e0592b4893cd9b6d3eaaf984e50029897275
Denzell Ford <fdenzell@gmail.com>
2021-01-02 02:29:24 -0600

    the goods

--- RisingStar/models/users.py
+++ RisingStar/models/users.py
@@ -7,6 +7,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
